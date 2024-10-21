c3cdd1d6827d13b3ab9901d1df7cac9985204b5e
ukattah <91033515+ukattah@users.noreply.github.com>
2024-10-20 21:19:13 -0400

    update to enemy.rs

---
+++ src/enemy.rs
@@ -0,0 +1,107 @@
+use bevy::prelude::*;
+use crate::GameState;
+use crate::map::Wall;
+
+const TILE_SIZE: u32 = 144;
+const ENEMY_SIZE: u32 = 144;
+const ENEMY_SPEED: f32 = 50.0;
+const PACE_BOUNDARY: usize = 2;
+
+#[derive(Component)]
+pub struct Enemy{
+    direction: i32,
+    left_boundary: f32, 
+    right_boundary: f32,
+}
+
+pub struct EnemyPlugin;
+    
+impl Plugin for EnemyPlugin{
+    fn build(&self, app: &mut App){
+/*         app.add_systems(Startup, init_enemy);
+ */        app.add_systems(Update, enemy_pace.run_if(in_state(GameState::InGame)));
+    }
+
+}
+
+/*
+fn init_enemy(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+){
+    let texture_handle = asset_server.load("enemyPlaceHolder.png");
+    println!("Loading texture: {:?}", texture_handle);
+    
+    commands.spawn(SpriteBundle {
+        texture: texture_handle,
+        transform: Transform {
+            translation: Vec3::new(0.0, 0.0, 900.0),
+            ..default()
+        },
+        ..default()
+    })
+    .insert(Enemy {});
+}*/
+/* 
+
+fn init_enemy(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlasLayout>>,
+) {
+    // Define the starting position for the enemy
+    let position = Vec3::new(0.0, 0.0, 900.0);
+    
+    spawn_enemy(&mut commands, &asset_server, &mut texture_atlases, position);
+} */
+
+pub fn spawn_enemy(
+    commands: &mut Commands,
+    asset_server: &Res<AssetServer>,
+    texture_atlases: &mut ResMut<Assets<TextureAtlasLayout>>,
+    position: Vec3,
+) {
+    // load textures and create texture atlases
+    let enemy_texture_handle = asset_server.load("enemyPlaceHolder.png");
+    let enemy_layout = TextureAtlasLayout::from_grid(UVec2::splat(ENEMY_SIZE), 1, 1, None, None);
+    let enemy_layout_handle = texture_atlases.add(enemy_layout);
+
+    let left_boundary = position.x - (TILE_SIZE as f32 * PACE_BOUNDARY as f32);
+    let right_boundary = position.x + (TILE_SIZE as f32 * PACE_BOUNDARY as f32);
+
+    commands.spawn((
+        SpriteBundle {
+            texture: enemy_texture_handle.clone(),
+            transform: Transform {
+                translation: position,
+                ..default()
+            },
+            ..default()
+        },
+        TextureAtlas {
+            index: 0, 
+            layout: enemy_layout_handle.clone(),
+        },
+        Enemy  {
+            direction: 1,
+            left_boundary,
+            right_boundary,
+        },
+    ));
+}
+
+fn enemy_pace(
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &mut Enemy)>,
+){
+    for (mut transform, mut enemy) in query.iter_mut() {
+        transform.translation.x += enemy.direction as f32 * ENEMY_SPEED * time.delta_seconds();
+        //turn if needed
+        if transform.translation.x > enemy.right_boundary {
+            enemy.direction = -1; 
+        } else if transform.translation.x < enemy.left_boundary {
+            enemy.direction = 1;
+        }
+        transform.translation.z = 900.0
+    }
+}
